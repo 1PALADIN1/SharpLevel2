e5c08d3f8b914fcb209fe9a771d34cad14b5d5f2
1PALADIN1 <sonikx94@yandex.ru>
2018-11-28 10:09:02 +0300

    init repo

---
+++ SharpLesson1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpLesson1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Привет, мир!");
+
+            Console.ReadKey();
+        }
+    }
+}
