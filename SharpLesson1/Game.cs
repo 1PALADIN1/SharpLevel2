3fa3e1a29712facc13b57ead487db222affcf339
1PALADIN1 <sonikx94@yandex.ru>
2018-11-28 10:39:01 +0300

    Добавил класс Game и подключил его к форме

---
+++ SharpLesson1/Game.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace SharpLesson1
+{
+    class Game
+    {
+        private static BufferedGraphicsContext _context;
+        public static BufferedGraphics buffer;
+
+        public static int Height { get; set; }
+        public static int Width { get; set; }
+
+        static Game()
+        {
+
+        }
+
+        public static void Init(Form form)
+        {
+            // Графическое устройство для вывода графики            
+            Graphics g;
+
+            _context = BufferedGraphicsManager.Current;
+            g = form.CreateGraphics();
+            Width = form.ClientSize.Width;
+            Height = form.ClientSize.Height;
+
+            // Связываем буфер в памяти с графическим объектом, чтобы рисовать в буфере
+            buffer = _context.Allocate(g, new Rectangle(0, 0, Width, Height));
+        }
+
+        //отрисовка
+        public static void Draw()
+        {
+            buffer.Graphics.Clear(Color.Black);
+            buffer.Graphics.DrawRectangle(Pens.White, new Rectangle(100, 100, 200, 200));
+            buffer.Graphics.FillEllipse(Brushes.Wheat, new Rectangle(100, 100, 200, 200));
+            buffer.Render();
+        }
+    }
+}
--- SharpLesson1/Program.cs
+++ SharpLesson1/Program.cs
@@ -5,16 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace SharpLesson1 {
-    class Program {
-        static void Main(string[] args) {
-            Form form = new Form {
+namespace SharpLesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form = new Form
+            {
                 Width = 800,
                 Height = 600
             };
-            //Game
+            Game.Init(form);
             form.Show();
-            //Game
+            Game.Draw();
             Application.Run(form);
         }
     }
