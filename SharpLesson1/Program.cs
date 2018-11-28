8677ff290fa950ce733f0265fd2de1cfbfa395c6
1PALADIN1 <sonikx94@yandex.ru>
2018-11-28 10:20:27 +0300

    Добавил форму

--- SharpLesson1/Program.cs
+++ SharpLesson1/Program.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SharpLesson1 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Привет, мир!");
-
-            Console.ReadKey();
+            Form form = new Form {
+                Width = 800,
+                Height = 600
+            };
+            //Game
+            form.Show();
+            //Game
+            Application.Run(form);
         }
     }
 }
