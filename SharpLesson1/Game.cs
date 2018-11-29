b918f92e8d9dc1cb850b210d65857784f5f39bec
1PALADIN1 <sonikx94@yandex.ru>
2018-11-29 12:14:38 +0300

    Поправил класс Game

--- SharpLesson1/Game.cs
+++ SharpLesson1/Game.cs
@@ -13,6 +13,8 @@ namespace SharpLesson1
         private static BufferedGraphicsContext _context;
         public static BufferedGraphics buffer;
 
+        public static BaseObject[] _objs;
+
         public static int Height { get; set; }
         public static int Width { get; set; }
 
@@ -20,6 +22,16 @@ namespace SharpLesson1
         {
 
         }
+
+        public static void Load()
+        {
+            _objs = new BaseObject[30];
+            for (int i = 0; i < _objs.Length; i++)
+            {
+                _objs[i] = new BaseObject(new Point(600, i * 20), new Point(15 - i, 15 - i), new Size(20, 20));
+            }
+        }
+
 
         public static void Init(Form form)
         {
@@ -33,6 +45,18 @@ namespace SharpLesson1
 
             // Связываем буфер в памяти с графическим объектом, чтобы рисовать в буфере
             buffer = _context.Allocate(g, new Rectangle(0, 0, Width, Height));
+
+            Load();
+
+            Timer timer = new Timer { Interval = 100 };
+            timer.Start();
+            timer.Tick += Timer_Tick;
+        }
+
+        private static void Timer_Tick(object sender, EventArgs e)
+        {
+            Draw();
+            Update();
         }
 
         //отрисовка
@@ -42,6 +66,18 @@ namespace SharpLesson1
             buffer.Graphics.DrawRectangle(Pens.White, new Rectangle(100, 100, 200, 200));
             buffer.Graphics.FillEllipse(Brushes.Wheat, new Rectangle(100, 100, 200, 200));
             buffer.Render();
+
+            //отрисовка массива объект
+            buffer.Graphics.Clear(Color.Black);
+            foreach (BaseObject obj in _objs)
+                obj.Draw();
+            buffer.Render();
+        }
+
+        public static void Update()
+        {
+            foreach (BaseObject obj in _objs)
+                obj.Update();
         }
     }
 }
