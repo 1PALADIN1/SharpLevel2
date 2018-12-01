f1d3bd9e98b85685b99979d7c6c0f4a7e88edd8f
1PALADIN1 <sonikx94@yandex.ru>
2018-12-01 23:13:33 +0300

    Задание 1

---
+++ SharpLesson2/FixEmployee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpLesson2
+{
+    /// <summary>
+    /// Класс сотрудников с фиксированной оплатой
+    /// </summary>
+    class FixEmployee : BaseEmployee
+    {
+        private double salary; //зарплата
+
+        /// <summary>
+        /// Инициализация сотрудника с фиксированной оплатой
+        /// </summary>
+        public FixEmployee()
+        {
+            salary = 0.0;
+        }
+
+        /// <summary>
+        /// Инициализация сотрудника с фиксированной оплатой
+        /// </summary>
+        /// <param name="salary">Зарплата</param>
+        public FixEmployee(double salary)
+        {
+            this.salary = salary;
+        }
+        
+        /// <summary>
+        /// Считает месячную заработную плату сотрудника с фиксированной оплатой
+        /// </summary>
+        /// <returns>Возращает сумму месячного оклада</returns>
+        public override double CountSalary()
+        {
+            return salary;
+        }
+    }
+}
---
+++ SharpLesson2/HourEmployee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpLesson2
+{
+    /// <summary>
+    /// Класс сотрудников с почасовой оплатой
+    /// </summary>
+    class HourEmployee : BaseEmployee
+    {
+        private double hourRate; //почасовая ставка
+
+        public double HourRate
+        {
+            get => hourRate;
+        }
+
+        /// <summary>
+        /// Инициализация сотрудника с почасовой оплатой
+        /// </summary>
+        public HourEmployee()
+        {
+            hourRate = 0.0;
+        }
+
+        /// <summary>
+        /// Инициализация сотрудника с почасовой оплатой
+        /// </summary>
+        /// <param name="hourRate">Почасовая ставка</param>
+        public HourEmployee(double hourRate)
+        {
+            this.hourRate = hourRate;
+        }
+        
+        /// <summary>
+        /// Считает месячную заработную плату сотрудника с почасовой оплатой
+        /// </summary>
+        /// <returns>Возращает сумму месячного оклада</returns>
+        public override double CountSalary()
+        {
+            return 20.8 * 8 * HourRate;
+        }
+    }
+}
